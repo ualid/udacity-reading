dfc2c3a15f2c153f589ebd93890480e37f42dad1
Ualid Oliveira <ualid@live.com>
2018-12-04 23:16:43 -0300

    Criação de componentes parte 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Panel from './Panel';
+import CardSimple from './CardSimple';
 
 class App extends Component {
   render() {
+    const test = [1, 2, 3];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+          <CardSimple >
+          {test.map((item, index) => (
+                <div className="row" key={index}>
+                <Panel />
+              </div>
+                  ))}
+            </CardSimple >     
       </div>
     );
   }
